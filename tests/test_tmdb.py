d13883017c98809b95c9089bb942c04c5344e36a
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-08-13 17:47:57 +0200

    Tests

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template, request, redirect
-import tmdb_client
+from movies_catalogue import tmdb_client
 import pytest
+from unittest.mock import Mock
 
-def test_get_single_movie(movie_id):
-    details = tmdb_client.get_single_movie(movie_id)
-    assert details is not None
+def test_get_single_movie():
+    single_movie = tmdb_client.get_single_movie(movie_id=2)
+    assert single_movie is not None
 
-def test_get_movie_images(movie_id):
-    movie_images_path = "backdrop_path"
-    expected_default_size = 'w780'
-    images_url = tmdb_client.get_movie_images(movie_id=movie_id)
-    assert expected_default_size in images_url
+def test_get_single_movie_cast(monkeypatch):
+   mock_single_movie_cast = ['Movie 1', 'Movie 2']
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_single_movie_cast
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
 
-def test_get_single_movie_cast(movie_id):
-    cast = tmdb_client.get_single_movie_cast(movie_id)
-    assert cast is not None
+
+   single_movie_cast = tmdb_client.get_single_movie_cast(movie_id=2)
+   assert single_movie_cast == mock_single_movie_cast
+
+
+# def test_get_single_movie_cast():
+#     single_movie_cast = tmdb_client.get_single_movie_cast(movie_id=3)
+#     assert single_movie_cast is not None
+
+# def test_get_movie_images():
+#     backdrop_path = "backdrop_path"
+#     expected_default_size = 'w780'
+#     images_url = tmdb_client.get_movie_images(movie_id=2)
+#     assert expected_default_size in images_url
 
 
 
