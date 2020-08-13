a63d2ef4968b3eca95e41115d176be8d1fd4553e
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-08-13 11:45:53 +0200

    Tests

---
+++ tests/test_tmdb.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+import tmdb_client
+
+def test_get_single_movie(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    assert details is not None
+
+def test_get_movie_images(movie_id):
+    movie_images_path = "backdrop_path"
+    expected_default_size = 'w780'
+    images_url = tmdb_client.get_movie_images(movie_id=movie_id)
+    assert expected_default_size in images_url
+
+def test_get_single_movie_cast(movie_id):
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    assert cast is not None
+
+
+
