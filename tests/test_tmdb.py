e0cb42b9335f1765586630188bd27d960b2e8d92
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-08-13 18:39:53 +0200

    Tests

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -14,20 +14,16 @@ def test_get_single_movie_cast(monkeypatch):
    response.json.return_value = mock_single_movie_cast
    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
 
-
    single_movie_cast = tmdb_client.get_single_movie_cast(movie_id=2)
    assert single_movie_cast == mock_single_movie_cast
 
+def test_get_single_movie_cast():
+    single_movie_cast = tmdb_client.get_single_movie_cast(movie_id=3)
+    assert single_movie_cast is not None
 
-# def test_get_single_movie_cast():
-#     single_movie_cast = tmdb_client.get_single_movie_cast(movie_id=3)
-#     assert single_movie_cast is not None
-
-# def test_get_movie_images():
-#     backdrop_path = "backdrop_path"
-#     expected_default_size = 'w780'
-#     images_url = tmdb_client.get_movie_images(movie_id=2)
-#     assert expected_default_size in images_url
+def test_get_movie_images():
+    images_url = tmdb_client.get_movie_images(movie_id=2)
+    assert images_url is not None
 
 
 
